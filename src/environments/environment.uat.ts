0b736b9b82efb9123c4f507a963c44b11ba7fdc9
Ravindranath Chavalam <ravindranath.c@qentelli.com>
2022-04-14 16:45:24 +0530

    fix uat url

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     environmentName: 'UAT',
-    apiUrl: 'https://pricingengineuat.azurewebsites.net/api'
+    apiUrl: 'https://pricingengineapiuat.azurewebsites.net/api'
 };
